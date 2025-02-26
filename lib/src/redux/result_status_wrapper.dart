36a357be7c884ca189766ce26ea89b8a28f12598
Shohjahon <komiljonovshohjahon@gmail.com>
2025-02-26 21:35:36 +0500

    Changes committed on 2025-02-26 21:35:36

--- lib/src/redux/result_status_wrapper.dart
+++ lib/src/redux/result_status_wrapper.dart
@@ -1,4 +1,3 @@
-import 'package:async_redux/async_redux.dart' as ar;
 import 'package:flutter/material.dart';
 import 'package:myspace_data/myspace_data.dart';
 
@@ -9,20 +8,23 @@ class ResultStatusWrapper<St> extends StatelessWidget {
     required this.builder,
     this.onDispose,
     this.onInitialBuild,
+    this.onDidChange,
   });
 
-  final ar.StoreConverter<St, StateResult> converter;
+  final StateResult Function(St state) converter;
   final Widget Function(BuildContext context, StateResult result) builder;
-  final ar.OnDisposeCallback<St>? onDispose;
-  final ar.OnInitialBuildCallback<St, StateResult>? onInitialBuild;
+  final void Function(BuildContext context)? onDispose;
+  final void Function(BuildContext context, StateResult result)? onInitialBuild;
+  final void Function(BuildContext context, St state, StateResult result)? onDidChange;
 
   @override
   Widget build(BuildContext context) {
     return StoreConnector<St, StateResult>(
       converter: converter,
+      builder: builder,
       onInitialBuild: onInitialBuild,
       onDispose: onDispose,
-      builder: builder,
+      onDidChange: onDidChange,
     );
   }
 }
--- lib/src/redux/store_connector.dart
+++ lib/src/redux/store_connector.dart
@@ -4,27 +4,27 @@ import 'package:flutter/widgets.dart';
 class StoreConnector<St, Model> extends StatelessWidget {
   const StoreConnector({
     super.key,
-    this.converter,
+    required this.converter,
     required this.builder,
     this.onDispose,
     this.onInitialBuild,
     this.onDidChange,
   });
 
-  final ar.StoreConverter<St, Model>? converter;
-  final ar.ViewModelBuilder<Model> builder;
-  final ar.OnDisposeCallback<St>? onDispose;
-  final ar.OnInitialBuildCallback<St, Model>? onInitialBuild;
-  final ar.OnDidChangeCallback<St, Model>? onDidChange;
+  final Model Function(St state) converter;
+  final Widget Function(BuildContext context, Model vm) builder;
+  final void Function(BuildContext context)? onDispose;
+  final void Function(BuildContext context, Model vm)? onInitialBuild;
+  final void Function(BuildContext context, St state, Model vm)? onDidChange;
 
   @override
   Widget build(BuildContext context) {
     return ar.StoreConnector<St, Model>(
-      converter: converter,
-      onDispose: onDispose,
-      onInitialBuild: onInitialBuild,
-      onDidChange: onDidChange,
+      converter: (store) => converter(store.state),
       builder: builder,
+      onDispose: onDispose != null ? (store) => onDispose!(context) : null,
+      onInitialBuild: onInitialBuild != null ? (ctx, store, vm) => onInitialBuild!(ctx ?? context, vm) : null,
+      onDidChange: onDidChange != null ? (ctx, store, vm) => onDidChange!(ctx ?? context, store.state, vm) : null,
     );
   }
 }
