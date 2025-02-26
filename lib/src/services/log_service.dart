1e77bda925092eada5258e9d4bbe385710c294b2
Shohjahon <komiljonovshohjahon@gmail.com>
2025-02-26 20:53:54 +0500

    Changes committed on 2025-02-26 20:53:54

--- lib/src/services/log_service.dart
+++ lib/src/services/log_service.dart
@@ -3,7 +3,21 @@ import 'package:logger/logger.dart';
 abstract class LogService {
   final Logger logger;
 
-  LogService([Logger? logger]) : logger = logger ?? Logger(printer: PrettyPrinter());
+  LogService([Logger? logger])
+      : logger = logger ??
+            Logger(
+              printer: PrettyPrinter(
+                methodCount: 2, // Number of method calls to be displayed
+                errorMethodCount: 8, // Number of method calls if stacktrace is provided
+                lineLength: 120, // Width of the output
+                colors: true, // Colorful log messages
+                printEmojis: true, // Print an emoji for each log message
+                // Should each log print contain a timestamp
+                dateTimeFormat: DateTimeFormat.onlyTime,
+              ),
+              output: null,
+              filter: null,
+            );
 
   void info(dynamic message) {
     logger.i(message);
