142689d24efc5259197b8410edc8db3413bb225e
Shohjahon <komiljonovshohjahon@gmail.com>
2025-03-03 07:38:12 +0500

    Changes committed on 2025-03-03 07:38:12

--- lib/src/redux.dart
+++ lib/src/redux.dart
@@ -2,10 +2,10 @@ export 'package:async_redux/async_redux.dart' show Store;
 
 export 'redux/action.dart';
 export 'redux/store.dart';
-export 'redux/store_provider.dart';
-export 'redux/state_connector.dart';
-export 'redux/state_result_connector.dart';
-export 'redux/dispatch_connector.dart';
+export 'redux/ui/store_provider.dart';
+export 'redux/ui/state_connector.dart';
+export 'redux/ui/state_result_connector.dart';
+export 'redux/ui/dispatch_connector.dart';
 
 export 'redux/di/di.dart';
 
